c065bd9a7ea70b259da3f51e6a2c369ebafd2f00
Wade Barnes <wade@neoterictech.ca>
2022-10-19 08:40:05 -0700

    Update backup configurations.
    - DC: Add parameters for wallt and api databases.
    - BC: Update to use the base image specified by the dockerfile rather than an override.
    
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -5,5 +5,5 @@ export GIT_REF="main"
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
 # with the values of GIT_URI and GIT_REF
-export skip_git_overrides=""
+export skip_git_overrides="aries-endorser-backup-build.yaml"
 export ignore_templates=""
